1eaf7c0b366dd14ce5421f77232a91dcdddac552
Thijmen-Jansen <t.jansen24@student.han.nl>
2025-04-04 11:39:00 +0200

    RetryLevelButton, Tilemaps verbeterd, GameOver + LevelComplete Text toegevoegd
    -RetryLevelButton verbeterd
    -TileMaps verbeterd
    -GameOver + LevelComplete Text

---
+++ src/main/java/org/example/text/GameOverText.java
@@ -0,0 +1,17 @@
+package org.example.text;
+
+import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.entities.impl.TextEntity;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+
+public class GameOverText extends TextEntity {
+
+    public GameOverText(Coordinate2D initialLocation) {
+        super(initialLocation, "Game Over");
+        setFont(Font.font("Roboto", FontWeight.NORMAL, 50));
+        setFill(Color.RED);
+    }
+
+}
